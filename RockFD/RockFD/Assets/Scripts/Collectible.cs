5d3ca13eb341687a95a8c6a4258ea6ab497415df
Mentosento <Brotherflygaming@gmail.com>
2020-05-24 06:12:38 -0400

    Collectible Initialization
    Collectible layout, switch for the assumed health pickup and bomb counts.

---
+++ RockFD/RockFD/Assets/Scripts/Collectible.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    //Variable
+    public int bombCharge;
+    public GameObject player;
+    [SerializeField]
+    private int healthPickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bombCharge = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        Collection();
+    }
+
+    void Collection()
+    {
+        switch (gameObject.tag)
+        {
+            case "Bomb":
+                bombCharge += 1;
+                break;
+            case "Health":
+                HealPlayer();
+                break;
+        }
+    }
+
+    void HealPlayer()
+    {
+        player.playerHealth += healthPickup;
+    }
+
+}
