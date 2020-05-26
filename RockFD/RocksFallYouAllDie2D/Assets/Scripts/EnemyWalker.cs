9dbd7f48db0669225d583bf67ab3ac6957bfeb08
Mentosento <Brotherflygaming@gmail.com>
2020-05-26 04:58:01 -0400

    Walker Prefab Trigger Setup
    Trigger properly calls, walker has 2 colliders, the central circle collider, and a larger box collider.  The box collider will act as trigger and deal damage, while the inner collider will prevent the player from moving through the enemy.

---
+++ RockFD/RocksFallYouAllDie2D/Assets/Scripts/EnemyWalker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check that the player entered trigger
+        //Check that the player is not currently in I-frames
+        //Deal damage to player health
+        Debug.Log("Player Entered Walker Trigger");
+    }
+}
