1643b893c78cd4830a60966d31d0318828a31a60
Mentosento <Brotherflygaming@gmail.com>
2020-05-31 23:46:21 -0400

    Prep For Projectiles & Pause
    Adding functionality to the pause menu which temporarily takes the projectiles out of physics simulation until the options menu is turned back off.

--- RockFD/RocksFallYouAllDie2D/Assets/Scripts/PauseFunctionality.cs
+++ RockFD/RocksFallYouAllDie2D/Assets/Scripts/PauseFunctionality.cs
@@ -6,13 +6,14 @@ public class PauseFunctionality : MonoBehaviour
 {
     private GameObject player;
     private bool playerPaused;
-
+    private Queue<GameObject> projectileDict;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         playerPaused = player.GetComponent<Player>().isPaused;
+        projectileDict = ObjectPooler.Instance.poolDict["projectile"];
     }
 
     // Update is called once per frame
@@ -20,4 +21,29 @@ public class PauseFunctionality : MonoBehaviour
     {
         
     }
+
+    void PauseFunction()
+    {
+        if (playerPaused != true)
+        {
+            foreach (GameObject projectile in projectileDict)
+            {
+                projectile.GetComponent<Rigidbody2D>().simulated = true;
+            }
+        }
+        else if (playerPaused == true)
+        {
+            foreach(GameObject projectile in projectileDict)
+            {
+                projectile.GetComponent<Rigidbody2D>().simulated = false;
+            }
+            //Turning off the physics simulation without a for each loop.
+            //GameObject currProjectile = projectileDict.Dequeue();
+            //currProjectile.GetComponent<Rigidbody2D>().simulated = false;
+            //projectileDict.Enqueue(currProjectile);
+        }
+    }
+
+
+
 }
