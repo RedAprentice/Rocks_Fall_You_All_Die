128822a58ad724646417a882f5c57687ddf4823c
Mentosento <Brotherflygaming@gmail.com>
2020-08-28 01:35:54 -0400

    Prefab adjustments, Tags, Damage Functionality
    Adjustments to prefabs for proper tags on Player and Projectile. Added Projectile tag.
    
    Enemy now has the functionality to deal damage. Slight issue with the Utils.hit not properly dealing the damage, though it can be dealt when written without it. Fixing shortly.

--- RockFD/RocksFallYouAllDie2D/Assets/Scripts/EnemyWalker.cs
+++ RockFD/RocksFallYouAllDie2D/Assets/Scripts/EnemyWalker.cs
@@ -8,11 +8,16 @@ public class EnemyWalker : MonoBehaviour
     private Vector2 playerPos;
     [SerializeField]
     private float eMoveSpeed = 2.0f;
+    private int damage = 1;
+    [SerializeField]
+    private int eHealth;
+    private Player pCS;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("PlaceholderCharacterSprite");
+        pCS = player.GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -24,9 +29,17 @@ public class EnemyWalker : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         //Check that the player entered trigger
+        if(other.tag == "Player")
+        {
+            Utils.hit(ref pCS.hp, damage);
+        }
+        if(other.tag == "Projectile")
+        {
+            Utils.hit(ref eHealth, pCS.projDamage);
+        }
         //Check that the player is not currently in I-frames
         //Deal damage to player health
-        Debug.Log("Player Entered Walker Trigger");
+
     }
 
 
@@ -40,7 +53,7 @@ public class EnemyWalker : MonoBehaviour
         //    eMoveSpeed += (Time.deltaTime / 8);
         //}
 
-        if (Vector2.Distance(transform.position, playerPos) >= 1.0f)
+        if (Vector2.Distance(transform.position, playerPos) >= 0.7f)
         {
             transform.position = Vector2.MoveTowards(transform.position, playerPos, eMoveSpeed * Time.deltaTime);
         }
